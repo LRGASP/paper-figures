e471f552962f2cd4219739f1b008f2a7ad5ec169
Mark Diekhans <markd@ucsc.edu>
2023-12-24 21:33:26 -0800

    commit extended data and supplemental figures

---
+++ figure-rules.mk
@@ -0,0 +1,18 @@
+.SECONDARY:
+.PHONY: all clean realclean
+tmpDir = tex.tmp
+
+all:: ${DOCS:%=%.pdf}
+
+latexopts = -file-line-error -halt-on-error -output-directory=${tmpDir}
+
+%.pdf: %.tex Makefile ../figure-rules.mk ../figure-defs.tex ${DEPENDS} 
+	@mkdir -p ${tmpDir}
+	pdflatex ${latexopts} -jobname $*.tmp \\nonstopmode\\input $< </dev/null
+	mv -f ${tmpDir}/$*.tmp.pdf $@
+
+clean:
+	rm -rf ${tmpDir}
+
+realclean: clean
+	rm -f ${DOCS:%=%.pdf}
